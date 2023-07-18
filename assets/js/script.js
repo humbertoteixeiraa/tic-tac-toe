94048068de95d454ae59136b69de8148e746a898
Humberto Teixeira <humberto.quim@gmail.com>
2023-07-18 16:56:58 -0300

    Update script.js

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,7 +14,27 @@ reset();
 //EVENTS
 document.querySelector('.reset').addEventListener('click', reset);
 
+document.querySelector('div[data-item=a1]').addEventListener('click', itemClick);
+document.querySelector('div[data-item=a2]').addEventListener('click', itemClick);
+document.querySelector('div[data-item=a3]').addEventListener('click', itemClick);
+document.querySelector('div[data-item=b1]').addEventListener('click', itemClick);
+document.querySelector('div[data-item=b2]').addEventListener('click', itemClick);
+document.querySelector('div[data-item=b3]').addEventListener('click', itemClick);
+document.querySelector('div[data-item=c1]').addEventListener('click', itemClick);
+document.querySelector('div[data-item=c2]').addEventListener('click', itemClick);
+document.querySelector('div[data-item=c3]').addEventListener('click', itemClick);
+
 //FUNCTIONS
+function itemClick(event) {
+    let item = event.target.getAttribute('data-item');
+    
+    if(square[item] === '') {
+        square[item] = player;
+        renderSquare();
+        tooglePlayer();
+    }
+}
+
 function reset() {
     warning = '';
 
@@ -39,6 +59,7 @@ function reset() {
 function renderSquare() {
     for(let i in square) {
         let item = document.querySelector(`div[data-item=${i}]`);
+
         if(square[i] !== '') {
             item.innerHTML = square[i];
         } else {
@@ -50,4 +71,13 @@ function renderSquare() {
 function renderInfo() {
     document.querySelector('.vez').innerHTML = player;
     document.querySelector('.resultado').innerHTML = warning;
+}
+
+function tooglePlayer() {
+    if(player === 'X') {
+        player = 'O';
+    } else {
+        player = 'X';
+    }
+    renderInfo();
 }
